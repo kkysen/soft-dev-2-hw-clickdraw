bb2e7bac87df4d4e3ff69aeb31859458bcc18b7f
Khyber Sen <kkysen@gmail.com>
2018-02-05 17:39:06 -0500

    now you can connect the dots and change coordinate systems

--- clickdraw.js
+++ clickdraw.js
@@ -1,4 +1,5 @@
 (function(parent) {
+    "use strict";
     
     const div = document.createElement("center");
     parent.appendChild(div);
@@ -14,13 +15,17 @@
     const buttonsDiv = document.createElement("div");
     div.appendChild(buttonsDiv);
     
-    const toggleButton = document.createElement("button");
-    buttonsDiv.appendChild(toggleButton);
+    const toggleShapeButton = document.createElement("button");
+    buttonsDiv.appendChild(toggleShapeButton);
+    
+    const toggleCoordButton = document.createElement("button");
+    buttonsDiv.appendChild(toggleCoordButton);
     
     const clearButton = document.createElement("button");
     buttonsDiv.appendChild(clearButton);
     
-    toggleButton.innerText = "Toggle";
+    toggleShapeButton.innerText = "Toggle Shape and Color";
+    toggleCoordButton.innerText = "Toggle Coordinate System";
     clearButton.innerText = "Clear";
     
     canvas.style.border = "1px solid black";
@@ -29,39 +34,63 @@
     canvas.height = height;
     
     const context = canvas.getContext("2d");
+    context.beginPath();
     
-    let stateIndex = 0;
+    let shapeIndex = 0;
+    let coordIndex = 0;
     
-    const stateFunctions = [
+    const shapeFunctions = [
         function redCircle(x, y) {
             const radius = 20;
-            context.beginPath();
-            context.fillStyle = "red";
+            context.fillStyle = context.strokeStyle = "red";
             context.arc(x, y, radius, 0, 2 * Math.PI);
             context.fill();
-            context.closePath();
         },
         function blueSquare(x, y) {
             const width = 40;
             const height = 40;
-            context.fillStyle = "blue";
+            context.fillStyle = context.strokeStyle = "blue";
             context.fillRect(x - width * 0.5, y - height * 0.5, width, height);
         },
     ];
     
-    toggleButton.addEventListener("click", e => {
+    const coordNames = [
+        "screen",
+        "offset",
+        "client",
+        "page",
+    ];
+    
+    const runShapeFunction = function(x, y) {
+        context.beginPath();
+        shapeFunctions[shapeIndex](x, y);
+        context.moveTo(x, y);
+    };
+    
+    toggleShapeButton.addEventListener("click", e => {
         e.preventDefault();
-        stateIndex = (stateIndex + 1) % stateFunctions.length;
+        shapeIndex = (shapeIndex + 1) % shapeFunctions.length;
+    });
+    
+    toggleCoordButton.addEventListener("click", e => {
+        e.preventDefault();
+        coordIndex = (coordIndex + 1) % coordNames.length;
     });
     
     clearButton.addEventListener("click", e => {
         e.preventDefault();
         context.clearRect(0, 0, canvas.width, canvas.height);
+        context.beginPath();
     });
     
     canvas.addEventListener("click", e => {
         e.preventDefault();
-        stateFunctions[stateIndex](e.offsetX, e.offsetY);
+        const coordName = coordNames[coordIndex] || "offset";
+        const x = e[coordName + "X"];
+        const y = e[coordName + "Y"];
+        context.lineTo(x, y);
+        context.stroke();
+        runShapeFunction(x, y);
     });
     
 })(document.body);
